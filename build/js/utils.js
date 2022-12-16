8c61cf483e921f08fc1612f5f9eef957fbc75d85
mictlantecutli <moisesparra82@hotmail.com>
2022-12-16 12:38:58 -0600

    changes in snowpack file

---
+++ build/js/utils.js
@@ -0,0 +1,16 @@
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
